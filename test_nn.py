f9ba719b04e27ff833c4060ea56c9d69a7c6d3d8
dimmddr <dmitrius-i@ya.ru>
2015-11-05 15:03:37 +0300

    End of rewriting, start testing.

--- test_nn.py
+++ test_nn.py
@@ -1,15 +1,18 @@
-import nn
-import numpy as np
-import os
-import datetime as dt
 import csv
+import datetime as dt
+import os
 import timeit
+
+import numpy as np
+
+import nn
+
 
 def test_init(seed = 16):
     global train_set_complete
     global test_set
     global lbl_test
-    train = np.loadtxt('train.csv', delimiter= ',', skiprows= 1, dtype= int)
+    train = np.loadtxt('train.csv', delimiter=',', skiprows=1, dtype=int)
     np.random.seed(seed)
     l = len(train)
     train_ind = np.random.choice(range(l), replace = False, size = np.floor(l * 0.75))
@@ -20,7 +23,8 @@ def test_init(seed = 16):
     test_set = np.delete(test_set, 0, 1)
     lbl_test[lbl_test == 0] = 10
 
-def write_results(result : list, test_name):
+
+def write_results(result: list, test_name):
     # Maybe I need to make it into start of file for easy tuning
     wd = os.getcwd()
     result_folder = wd + "\\results"
@@ -48,11 +52,13 @@ def write_results(result : list, test_name):
             writer.writerow(res)
     os.chdir(wd)
 
+
 # test for train set size
-def test_learning_size(min_size = 500, max_size = 5000, step_size = 500):
-    test_init()
+def test_learning_size(min_size=500, max_size=5000, step_size=500, init=False):
+    # I don't want to do it multiply times, read large file is long.
+    if not init:
+        test_init()
     
-    # test for hidden layer size
     res = []
     for ind in range(min_size, max_size, step_size):
         print(ind)
@@ -75,111 +81,95 @@ def test_learning_size(min_size = 500, max_size = 5000, step_size = 500):
         s = sum(predict_res == lbl_test)
         proc_n = s/len(lbl_test)
         print(proc_n)
-        res.append({ 'training_size' : ind,
-            'accuracy' : proc_n,
-            'learning_speed' : alfa,
-            'hidden_layer_size' : hl,
-            'time' : m_time
+        res.append({
+            'training_size': ind,
+            'accuracy': proc_n,
+            'learning_speed': alfa,
+            'hidden_layer_size': hl,
+            'time': m_time
         })
     write_results(res, "size_measurements")
     return res
 
+
 # test for hidden layer size
-def test_hidden_layer_size (min_size = 15, max_size = 50, step_size = 5):
-    
-    size = c()
-    m_time = c()
-    acc = c()
-    
-    test_init()
-    
-    # test for hidden layer size
-    res = data.frame(training_size = numeric(), accuracy = numeric(), 
-                      learning_speed = numeric(), user_time = numeric(),
-                      sys_time = numeric(), elapsed_time = numeric())
-    
-    for(hl in seq.int(min_size, max_size, step_size)) {
+def test_hidden_layer_size(min_size=15, max_size=50, step_size=5, init=False):
+    # I don't want to do it multiply times, read large file is long.
+    if not init:
+        test_init()
+
+    res = []
+    for hl in range(min_size, max_size, step_size):
         print(hl)
         ind = 3500
         alfa = 1
-        init(input_size = 28 * 28, hidden_size = hl, output_size = 10, alfa_ = alfa, seed = 123)
-        
-        train_set = train_set_complete[1:ind, ]
-        
-        lbl_train = train_set[, 1]
-        train_set[, 1] = NULL
-        lbl_train[lbl_train == 0] = 10 
-        
+        nn.init(input_size=28 * 28, hidden_size=hl, output_size=10, alfa_=alfa, seed=123)
+
+        train_set = train_set_complete[0:ind]
+
+        lbl_train = train_set[:, 0]
+        train_set = train_set[:, 1:]
+        lbl_train[lbl_train == 0] = 10
+
         print("Learn")
-        ptm = proc.time()
-        learning(x = train_set, lbl = lbl_train)
-        m_time = proc.time()
+        m_time = timeit.timeit(nn.learning(x=train_set, lbl=lbl_train))
         print("Predict")
-        predict_res = apply(test_set, 1, predict)
+        predict_res = []
+        for test in test_set:
+            predict_res.append(nn.predict(test))
         s = sum(predict_res == lbl_test)
-        proc_n = s/length(lbl_test)
-        # print(hl)
+        proc_n = s / len(lbl_test)
         print(proc_n)
-        new_row = data.frame(training_size = ind, 
-                              accuracy = proc_n, 
-                              learning_speed = alfa, 
-                              hidden_layer_size = hl,
-                              user_time = m_time["user.self"],
-                              sys_time = m_time["sys.self"], 
-                              elapsed_time = m_time["elapsed"])
-        res = rbind(res, new_row)
-    }
-    
+        res.append({
+            'training_size': ind,
+            'accuracy': proc_n,
+            'learning_speed': alfa,
+            'hidden_layer_size': hl,
+            'time': m_time
+        })
     write_results(res, "hidden_layer_size_measurements")
-    res
+    return res
+
 
 # test for train speed
-def test_learning_speed(min_speed = 0.1, max_speed = 2, step_size = 0.1):
-    
-    size = c()
-    m_time = c()
-    acc = c()
-    
-    test_init()
-    
-    res = data.frame(training_size = numeric(), accuracy = numeric(), 
-                      learning_speed = numeric(), user_time = numeric(),
-                      sys_time = numeric(), elapsed_time = numeric())
-    
-    for(alf in seq.int(min_speed, max_speed, step_size)) {
+def test_learning_speed(min_speed=0.1, max_speed=2, step_size=0.1, init=False):
+    # I don't want to do it multiply times, read large file is long.
+    if not init:
+        test_init()
+
+    res = []
+    for alf in range(min_speed, max_speed, step_size):
         print(alf)
         ind = 3500
         alfa = alf
         hl = 25
-        init(input_size = 28 * 28, hidden_size = hl, output_size = 10, alfa_ = alfa, seed = 123)
-        
-        train_set = train_set_complete[1:ind, ]
-        
-        lbl_train = train_set[, 1]
-        train_set[, 1] = NULL
-        lbl_train[lbl_train == 0] = 10 
-        
+        nn.init(input_size=28 * 28, hidden_size=hl, output_size=10, alfa_=alfa, seed=123)
+
+        train_set = train_set_complete[0:ind]
+
+        lbl_train = train_set[:, 0]
+        train_set = train_set[:, 1:]
+        lbl_train[lbl_train == 0] = 10
+
         print("Learn")
-        ptm = proc.time()
-        learning(x = train_set, lbl = lbl_train)
-        m_time = proc.time() - ptm
+        m_time = timeit.timeit(nn.learning(x=train_set, lbl=lbl_train))
         print("Predict")
-        predict_res = apply(test_set, 1, predict)
+        predict_res = []
+        for test in test_set:
+            predict_res.append(nn.predict(test))
         s = sum(predict_res == lbl_test)
-        proc_n = s/length(lbl_test)
-        # print(hl)
+        proc_n = s / len(lbl_test)
         print(proc_n)
-        new_row = data.frame(training_size = ind, 
-                              accuracy = proc_n, 
-                              learning_speed = alfa, 
-                              hidden_layer_size = hl,
-                              user_time = m_time["user.self"],
-                              sys_time = m_time["sys.self"], 
-                              elapsed_time = m_time["elapsed"])
-        res = rbind(res, new_row)
-    }
+        res.append({
+            'training_size': ind,
+            'accuracy': proc_n,
+            'learning_speed': alf,
+            'hidden_layer_size': hl,
+            'time': m_time
+        })
     write_results(res, "training_speed_measurements")
-    res
+    return res
+
 
 def test_all():
     print("Test learning size")
